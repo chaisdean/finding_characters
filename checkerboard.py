9bde569e312a329e175dc434521e14dc39939eb1
Chais Dean <capslock@Chaiss-MacBook-Pro.local>
2017-07-07 19:03:23 -0700

    checkerboard

---
+++ checkerboard.py
@@ -0,0 +1,11 @@
+star = '* '
+
+
+for star in range(0, 5):
+    print ("* * * * ")
+    for star in range(0, 4):
+        print (" * * * *")
+        for star in range(0, 2):
+            print ("* * * *")
+            for star in range(0, 1):
+                print (" * * * *")
